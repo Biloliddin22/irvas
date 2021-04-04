6a6e1a45cb079cfb968e7f2497a755da07093243
Biloliddin <biloliddin500dbmin@gmail.com>
2021-04-04 16:20:05 +0500

    upgrades

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,12 @@ window.addEventListener('DOMContentLoaded', () => {
     
     changeModalState(modalState);
     modals();
-    tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+    tabs(
+            '.glazing_slider ',
+            '.glazing_block',
+            '.glazing_content',
+            'active'
+        );
     tabs(
             '.decoration_slider',
             '.no_click',
